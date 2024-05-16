18fcb51b82cdaed8942b1658e23bc3dcd87f5576
zzyuri <yuriap200711@gmail.com>
2024-05-16 08:09:16 -0300

    Renomeei a view servico.blade.php para list_servico.blade.php e ajustei o controller

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -16,75 +16,56 @@ class ServicoController extends Controller
         $this->servico = new Servico();
     }
 
-    
-
-
-    /**
-     * Display a listing of the resource.
-     */
+    // INDEX
     public function index()
     {
         $servicos = $this->servico->all();
-        
-            
-        
-       
-        return view('pages/servicos/servico', ['servico' => $servicos]);
+
+        return view('pages/servicos/list_servico', ['servico' => $servicos]);
     }
-    
-    /**
-     * Show the form for creating a new resource.
-     */
+
+    // CREATE
     public function create()
     {
         return view('pages/servicos/create_servico');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    // STORE
     public function store(Request $request)
     {
-       
-       
+
         $created= $this->servico->create([
             'nome'=>$request->input('nome'),
             'tempo'=>$request->input('tempo'),
             'preco'=>$request->input('preco'),
             'user_id'=>Auth::user()->id,
-        
 
-            
         ]);
+
         if($created){
             return redirect()->back()->with('message', 'Funcionarios adicionado');
         };
 
         return redirect()->back()->with('message', 'erro');
+
     }
 
-    /**
-     * Display the specified resource.
-     */
+    // SHOW
     public function show(servico $servico)
     {
         return view('pages/servicos/show_servico', ['servico'=>$servico]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    // EDIT
     public function edit(servico $servico)
     {
         return view('pages/servicos/edit_servico',['servico' =>$servico]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    // UPDATE
     public function update(Request $request, servico $servico)
     {
-        { 
+        {
             $id = $servico->id;
             $update = $this->servico->where('id', $id)->update($request->except(['_token', '_method']));
             if($update){
@@ -92,16 +73,13 @@ class ServicoController extends Controller
                 return redirect()->back()->with('message', 'Update feito');
                 // $servicos = $this->servico->all();
                 // return view('pages/servicos/servico', ['servico' => $servicos])->with('message', 'Update feito');
-            
+
             }
-            
-            
+
         }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    // DELETE
     public function destroy(servico $servico)
     {
         $id = $servico->id;
@@ -109,3 +87,4 @@ class ServicoController extends Controller
         return redirect()->route('servicos.index');
     }
 }
+
